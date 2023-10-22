6a236361d9e65dc74a73a59cdbbf1b88f4abbac0
Mashkin E.V <mev.86@list.ru>
2023-10-22 22:06:33 +0300

    Initial commit

---
+++ src/js/__tests__/test_matchers.js
@@ -0,0 +1,18 @@
+import matchers from '../matchers';
+
+const heroes = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+
+const expected = [
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'мечник', health: 10 },
+];
+
+test('test', () => {
+  const result = matchers(heroes);
+  expect(result).toEqual(expected);
+});
